d3fc3e5334247e13b4cb81c51ad75591b887881c
ninadcoditas <ninad.virkar@coditas.com>
2020-09-04 20:07:01 +0530

    finished cast

--- src/app/custom-components/Cast.ts
+++ src/app/custom-components/Cast.ts
@@ -8,20 +8,45 @@ export class Cast extends LitElement {
         // console.log('called from cast component')
     }
 
+    show: boolean = false;
+
     static get styles() {
         return css`
-      li { 
+      .cast { 
           cursor: pointer; 
         }
+    
+        .cast-detail{
+            height:100%;
+
+        }
+
+        .hidden {
+  display: none;
+  -webkit-transition: display .5s ease;
+  
+}
+
+.active {
+  -webkit-transition: display .5s ease;
+}
+
+        
     `;
     }
 
+
     render() {
         return html`
-        <slot></slot>
-      <ul>
-       <li> <slot name="name"></slot> </li>
-      </li>
-        `
+
+        <slot></slot> 
+    <ul>
+    <li><slot name="name" > </slot> </li>
+        </ul>
+
+     `
     }
 }
+
+// <div class="cast" @click="${() => { this.show = !this.show }}> more < /div>
+// ${ this.show ? html`<p>shoow </p>` : html`<p>hide</p>` }
