fbc3d03c3dede3c9afb9b3e82b24708f1fafc713
ninad <ninad.virkar@coditas.com>
2020-09-08 10:58:48 +0530

    deleted all tests

--- src/app/services/theme.service.spec.ts
+++ src/app/services/theme.service.spec.ts
@@ -2,15 +2,3 @@ import { TestBed } from '@angular/core/testing';
 
 import { ThemeService } from './theme.service';
 
-describe('ThemeService', () => {
-  let service: ThemeService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ThemeService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
