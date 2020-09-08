ffd77d8363270e7cdd769dd07ac9ed522fcaca7d
ninad <ninad.virkar@coditas.com>
2020-09-09 00:14:54 +0530

    add component story

--- src/stories/add.stories.ts
+++ src/stories/add.stories.ts
@@ -7,41 +7,51 @@ import { ThemeService } from '../app/services/theme.service';
 import { StoreModule } from '@ngrx/store';
 import { authreducer } from '../app/reducers/auth.reducer'
 
+import { APP_BASE_HREF } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
-    AddComponent,
-    UpdateComponent,
-    HomeComponent,
-    MovieCardComponent,
-    MovieListComponent,
-    SignupComponent,
-    LoginComponent,
-    HeaderComponent
+    AddComponent
 } from './components';
+import { RouterModule } from '@angular/router';
 
 export default {
     title: 'Example/AddMovie',
     component: AddComponent,
     decorators: [
         moduleMetadata({
-            // declarations: [
-            //     HomeComponent,
-            //     UpdateComponent,
-            //     LoginComponent,
-            //     SignupComponent,
-            //     HeaderComponent,
-            //     MovieCardComponent,
-            //     MovieListComponent
-            // ],
+
             imports: [
                 CommonModule,
-
+                FontAwesomeModule,
+                RouterModule.forRoot([], { useHash: true }),
                 StoreModule.forRoot({
                     auth: authreducer
                 })],
-            providers: [ThemeService]
+            providers: [{ provide: APP_BASE_HREF, useValue: '/' }, ThemeService]
         }),
     ],
     argTypes: {
+        movie: {
+            "name": "The Dark Knight",
+            "genre": "Action,Crime,Drama",
+            "rating": 9.3,
+            "cast": [
+                "Christian Bale",
+                "Heath Ledger",
+                "Morgan Freeman"
+            ]
+        }
+    }
+} as unknown as Meta;
+
+const Template: Story<AddComponent> = (args: AddComponent) => ({
+    component: AddComponent,
+    props: args,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+    movie: {
         "name": "The Dark Knight",
         "genre": "Action,Crime,Drama",
         "rating": 9.3,
@@ -51,31 +61,5 @@ export default {
             "Morgan Freeman"
         ]
     }
-} as unknown as Meta;
-
-const Template: Story<AddComponent> = (args: AddComponent) => ({
-    component: AddComponent,
-    props: args,
-});
-
-export const Filled = Template.bind({});
-Filled.args = {
-    "name": "The Dark Knight",
-    "genre": "Action,Crime,Drama",
-    "rating": 9.3,
-    "cast": [
-        "Christian Bale",
-        "Heath Ledger",
-        "Morgan Freeman"
-    ]
 }
-
-export const Empty = Template.bind({});
-Empty.args = {
-    "name": "",
-    "genre": "",
-    "rating": "",
-    "cast": ""
-
-};
 
